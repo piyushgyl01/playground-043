d9aa1467e41505aa1df8f4092ed056936ed1a8a6
piyush.gyl <piyush2022ug@gmail.com>
2025-04-15 20:49:56 +0530

    routing & templating

---
+++ src/components/Taglist.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Taglist() {
+  return (
+    <div>Taglist</div>
+  )
+}
---
+++ src/hooks/usePost.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function usePost() {
+  return (
+    <div>usePost</div>
+  )
+}
